7251d5c9fc5bd5626904d7e4bf7cdf24a156385c
Victor <vjps1993@gmail.com>
2022-12-12 03:17:05 -0500

    img

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $("body").append(
-    ' <div class="chatbot-icon"> <div class="chatbot-msg"> <div class="chatbot-close-body"> <div class="chatbot-close">x</div> </div> <p>Hola ¿Te gustaría saber algo de la vida?</p> </div> <img src="/david.jpg" /> </div><div class="chatbot"> <div class="chatbot-body"></div> <div class="chatbot-submit"> <form id="form" method="POST"> <input type="text" name="prompt" id="prompt" /> <button type="submit"></button> </form> </div> </div>'
+    ' <div class="chatbot-icon"> <div class="chatbot-msg"> <div class="chatbot-close-body"> <div class="chatbot-close">x</div> </div> <p>Hola ¿Te gustaría saber algo de la vida?</p> </div> <img src="https://cdn.jsdelivr.net/gh/Lolichess/chatbot@ceeb2ee66ae6d98ae0a1939df3601606852390f2/public/david.jpg" /> </div><div class="chatbot"> <div class="chatbot-body"></div> <div class="chatbot-submit"> <form id="form" method="POST"> <input type="text" name="prompt" id="prompt" /> <button type="submit"></button> </form> </div> </div>'
   );
 
   $(".chatbot-icon").click(function () {
