b302be79f5ed6f6eddb8e3e2b8262a7c6c192f6f
Victor <vjps1993@gmail.com>
2022-12-12 02:46:33 -0500

    cors error

--- app.js
+++ app.js
@@ -3,23 +3,13 @@ const bodyParser = require("body-parser");
 const app = express();
 const path = require("path");
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 
 const { Configuration, OpenAIApi } = require("openai");
 
-app.use(express.static("public"));
+app.options("*", cors());
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
+app.use(express.static("public"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
