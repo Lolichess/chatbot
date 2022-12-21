3ffa3e1e15b3a174291ab1fd73e5f5a7609a6ef9
Victor <vjps1993@gmail.com>
2022-12-21 08:27:13 -0500

    cambios de texto

--- chatbot.js
+++ chatbot.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   $("body").append(
-    ' <div class="chatbot-icon"> <div class="chatbot-msg"> <div class="chatbot-close-body"> <div class="chatbot-close">x</div> </div> <p>Soy chatgpt puedes preguntar cualquier duda sobre qué es web3?</p> </div> <img src="https://cdn.jsdelivr.net/gh/Lolichess/chatbot@4c753f8ae8fadc024e3584a15c885b9f0566c43f/public/david.jpg" /> </div><div class="chatbot"><div class="chatbot-header"><div class="chatbot-minimize"></div></div> <div class="chatbot-body"></div> <div class="chatbot-submit"><div class="question-relative"><div class="question">¿Qué es Web 3?</div><div class="question">¿Qué es AI generativa?</div><div class="question">¿Cómo puedo orientar mi carrera hacia Blockchain?</div></div><div class="chatbot-writing">Benito Pérez Galdós escribiendo...</div> <form id="form" method="POST"> <input type="text" name="prompt" id="prompt" autocomplete="off" /> <button type="submit"></button> </form> </div> </div>'
+    ' <div class="chatbot-icon"> <div class="chatbot-msg"> <div class="chatbot-close-body"> <div class="chatbot-close">x</div> </div> <p>Soy chatgpt puedes preguntar cualquier duda sobre qué es web3?</p> </div> <img src="https://cdn.jsdelivr.net/gh/Lolichess/chatbot@4c753f8ae8fadc024e3584a15c885b9f0566c43f/public/david.jpg" /> </div><div class="chatbot"><div class="chatbot-header"><div class="chatbot-minimize"></div></div> <div class="chatbot-body"></div> <div class="chatbot-submit"><div class="question-relative"><div class="question">¿Qué es Web 3?</div><div class="question">¿Qué es AI generativa?</div><div class="question">¿Cómo puedo orientar mi carrera hacia Blockchain?</div></div><div class="chatbot-writing">ChatGPT escribiendo...</div> <form id="form" method="POST"> <input type="text" name="prompt" id="prompt" autocomplete="off" /> <button type="submit"></button> </form> </div> </div>'
   );
 
   $(".chatbot-icon").click(function (e) {
@@ -21,9 +21,7 @@ $(document).ready(function () {
   });
 
   $(".question").click(function () {
-    let values =
-      "prompt=" +
-      encodeURIComponent("Human: " + $(this).text() + "\nBenito Pérez Galdós:");
+    let values = "prompt=" + encodeURIComponent($(this).text());
 
     $(".chatbot-body").append(
       ' <div class="chatbot-question"> <div class="chatbot-img"> <img src="https://cdn.jsdelivr.net/gh/Lolichess/chatbot@ceeb2ee66ae6d98ae0a1939df3601606852390f2/public/user.jpg" /> </div> <div class="chatbot-text"> <p>' +
