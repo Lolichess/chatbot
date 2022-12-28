f250658149c6b672b3b4ce813a5f27aac677fd67
Victor <vjps1993@gmail.com>
2022-12-27 22:10:20 -0500

    change font-size

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ getValue = async (prompt) => {
     model: "text-davinci-003",
     prompt: prompt,
     temperature: 0.9,
-    max_tokens: 300,
+    max_tokens: 500,
     top_p: 1,
     frequency_penalty: 0,
     presence_penalty: 0.6,
